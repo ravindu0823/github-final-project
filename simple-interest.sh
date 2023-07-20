bdcd501e1e00e1775e194c65d8dd42072a063a21
R R D Perera <68367636+ravindu0823@users.noreply.github.com>
2023-07-20 15:42:48 +0530

    Update simple-interest.sh
    Added username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # ravindu0823
    # Input:
    # p, principal amount
    # t, time period in years
